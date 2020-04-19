c7b8bd8c4862566c73e54d978d5e53b7f85f13e0
Hyperion <lasagnes.tomate@gmail.com>
2020-04-19 14:51:37 +0200

    django: Execute django-admin startproject
    Signed-off-by: Hyperion <lasagnes.tomate@gmail.com>

--- HyperAnnales/HyperAnnales/wsgi.py
+++ HyperAnnales/HyperAnnales/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for HA3_0 project.
+WSGI config for HyperAnnales project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HA3_0.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HyperAnnales.settings')
 
 application = get_wsgi_application()
--- HyperAnnales/manage.py
+++ HyperAnnales/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HA3_0.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HyperAnnales.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
