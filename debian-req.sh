78ba7e465425677c311885d11b83c15a9a3545b3
Hyperion <lasagnes.tomate@gmail.com>
2020-04-21 13:03:01 +0200

    django(media): Add media files git

--- debian-req.sh
+++ debian-req.sh
@@ -21,16 +21,15 @@ sudo apt -y install git-core
 
 # Download static file, templates, database
 cd /home/HyperAnnales
-mkdir templates
-cd templates
 git clone git@github.com:Hyperion60/Templates_HA.git
 
 cd /home/HyperAnnales
-mkdir static
-cd static
 git clone git@github.com:Hyperion60/static_HA.git
 
 cd /home/HyperAnnales
-mkdir db
-cd db
 git clone git@github.com:Hyperion60/db_HA.git
+
+cd /
+mkdir media
+cd media
+git clone git@github.com:Hyperion60/media_HA.git
