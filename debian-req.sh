5edf0ebda4a61791945d23d2562f720fe55c28f9
Hyperion <lasagnes.tomate@gmail.com>
2020-04-21 14:30:25 +0200

    docker(fix): Sudo not found

--- debian-req.sh
+++ debian-req.sh
@@ -2,7 +2,7 @@
 
 #Python3.7 install
 sudo apt update
-sudo apt-get -y install build-essential zlib1g-dev libncurses5-dev libgdm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev curl
+sudo apt-get -y install build-essential zlib1g-dev libncurses5-dev libgdm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev curl sudo
 curl -O https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz
 tar -xf Python-3.7.3.tar.xz
 cd Python-3.7.3
