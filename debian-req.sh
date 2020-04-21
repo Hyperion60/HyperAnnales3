3850203070b4e0758b81c4a3ed74b8fc6b47c100
Hyperion <lasagnes.tomate@gmail.com>
2020-04-21 14:31:28 +0200

    docker(fix): Sudo not found

--- debian-req.sh
+++ debian-req.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
 #Python3.7 install
-sudo apt update
-sudo apt-get -y install build-essential zlib1g-dev libncurses5-dev libgdm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev curl sudo
+apt update
+apt-get -y install build-essential zlib1g-dev libncurses5-dev libgdm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev curl sudo
 curl -O https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz
 tar -xf Python-3.7.3.tar.xz
 cd Python-3.7.3
 ./configure --enable-optimizations
 make
-sudo make install
+make install
 
 cd ..
 rm -rf Python-3.7.3.tar.xz Python-3.7.3
 
 #Install pip for Python3.7
-sudo apt update && sudo apt -y install python3-pip libssl-dev
+apt update && apt -y install python3-pip libssl-dev
 
 # Install git
-sudo apt -y install git-core
+apt -y install git-core
 
 # Download static file, templates, database
 cd /home/HyperAnnales
