eb5c218e6cf7caf9dee2fe408e29b9981c5a4a33
Hyperion <lasagnes.tomate@gmail.com>
2020-04-23 19:43:45 +0200

    docker: update sqlite3 to mysql

--- HyperAnnales/settings.py
+++ HyperAnnales/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'HyperAnnales.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'db_user',
         'USER': 'root',
         'PASSWORD': 'qyD^Z<y>F9E86u5',
