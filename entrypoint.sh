5e8e9c907dc108728d7d95ee385f5a030d362e54
Hyperion <lasagnes.tomate@gmail.com>
2020-04-26 22:46:07 +0200

    django: add arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 set -e
 
 sleep 5
-uwsgi --wsgi-file "/home/HyperAnnales/HyperAnnales/wsgi.py" --http-socket 127.0.0.1:6094 --chdir "/home/HyperAnnales"
+uwsgi --plugin python --wsgi-file "/home/HyperAnnales/HyperAnnales/wsgi.py" --http-socket 0.0.0.0:6094 --chdir "/home/HyperAnnales"
