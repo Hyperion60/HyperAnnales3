dafcf69d477db7f0131fc12f4e194f298f486f8a
Hyperion <lasagnes.tomate@gmail.com>
2020-04-26 21:41:34 +0200

    django: add log file

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 set -e
 
 sleep 10
-uwsgi --wsgi-file "/home/HyperAnnales/HyperAnnales/wsgi.py" --http-socket 0.0.0.0:6094 --chdir "/home/HyperAnnales"
+uwsgi --wsgi-file "/home/HyperAnnales/HyperAnnales/wsgi.py" --http-socket 0.0.0.0:6094 --chdir "/home/HyperAnnales" --req-logger "file:/log/access.log" --logger "file:/log/error.log"
