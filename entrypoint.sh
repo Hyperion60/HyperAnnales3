5227e812d5609a0980194238c77be83b9260773b
Hyperion <lasagnes.tomate@gmail.com>
2020-04-26 14:57:43 +0200

    docker: restore network settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ sleep 10
 apt -y install tree
 cat HyperAnnales/HyperAnnales/uwsgi.ini
 tree HyperAnnales
-uwsgi --wsgi-file "/home/HyperAnnales/HyperAnnales/wsgi.py" --http-socket 127.0.0.1:6094 --chdir "/home/HyperAnnales"
+uwsgi --wsgi-file "/home/HyperAnnales/HyperAnnales/wsgi.py" --http-socket 0.0.0.0:6094 --chdir "/home/HyperAnnales"
