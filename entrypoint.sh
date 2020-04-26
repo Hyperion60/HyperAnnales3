84a38898367608e914c1d8883714cfd21e0e25b3
Hyperion <lasagnes.tomate@gmail.com>
2020-04-26 19:28:51 +0200

    docker: clean entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,4 @@
 set -e
 
 sleep 10
-apt -y install tree
-cat HyperAnnales/HyperAnnales/uwsgi.ini
-tree HyperAnnales
 uwsgi --wsgi-file "/home/HyperAnnales/HyperAnnales/wsgi.py" --http-socket 0.0.0.0:6094 --chdir "/home/HyperAnnales"
