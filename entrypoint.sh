8677cbc5febc60c760144243420e6f2ad6e3804e
Hyperion <lasagnes.tomate@gmail.com>
2020-04-26 14:41:26 +0200

    docker: add uwsgi inline

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,4 @@ sleep 10
 apt -y install tree
 cat HyperAnnales/HyperAnnales/uwsgi.ini
 tree HyperAnnales
+uwsgi --wsgi-file "/home/HyperAnnales/HyperAnnales/wsgi.py" --http-socket 127.0.0.1:6094 --chdir "/home/HyperAnnales"
