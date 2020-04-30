7ad9db57a6fe6c149ef4a0c074ea76491c9ba8d0
Hyperion <lasagnes.tomate@gmail.com>
2020-04-30 18:52:18 +0200

    django: Add views to serve static files

--- staticfiles/views.py
+++ staticfiles/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django_sendfile import sendfile
+
 
 # Create your views here.
+
+
+def static_index(request):
+    return HttpResponse(status=404)
+
+
+def serve_static(request, static):
+    return sendfile(request, static)
