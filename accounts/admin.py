8bb6bbfb21998fad66a13fe42501715c2fee01e0
Hyperion <lasagnes.tomate@gmail.com>
2020-05-03 19:21:09 +0200

    django(user): add search-field order

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,7 +5,7 @@ from accounts.models import Account
 
 class AccountAdmin(UserAdmin):
     list_display = ('username', 'email', 'date_joined', 'is_staff', 'is_contributor', 'is_active',)
-    search_fields = ('email', 'username', 'date_joined',)
+    search_fields = ('email', 'username', 'date_joined', 'is_active')
     readonly_fields = ('date_joined', 'last_login',)
     ordering = ('username',)
 
