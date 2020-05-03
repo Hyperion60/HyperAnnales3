6e12cfa094c2e29c6f098bfab754b2d761faa06e
Hyperion <lasagnes.tomate@gmail.com>
2020-05-03 19:18:25 +0200

    django(user): add default order

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,9 +4,10 @@ from accounts.models import Account
 
 
 class AccountAdmin(UserAdmin):
-    list_display = ('email', 'username', 'date_joined', 'is_staff', 'is_contributor', 'is_active')
-    search_fields = ('email', 'username', 'date_joined')
-    readonly_fields = ('date_joined', 'last_login')
+    list_display = ('email', 'username', 'date_joined', 'is_staff', 'is_contributor', 'is_active',)
+    search_fields = ('email', 'username', 'date_joined',)
+    readonly_fields = ('date_joined', 'last_login',)
+    ordering = ('username',)
 
     filter_horizontal = ()
     list_filter = ()
