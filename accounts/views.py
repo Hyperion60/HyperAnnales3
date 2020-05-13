a70e0517a51835e9950933969d8fd70ec5c3efbd
Hyperion <lasagnes.tomate@gmail.com>
2020-05-13 20:37:26 +0200

    django: Add authentication functions

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
 
-# Create your views here.
+from accounts.forms import RegistrationForm, AccountAuthenticationForm
+
+
+def registration_view(request):
+    context = {}
+    if request.POST:
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email')
+            raw_password = form.cleaned_data('password1')
+            account = authenticate(email=email, password=raw_password)
+            login(request, account)
+            return redirect('/')
+        else:
+            context['registration_form'] = form
+    else:
+        form = RegistrationForm()
+        context['registration_form'] = form
+    return render(request, 'account/register.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("/")
+
+
+def login_view(request):
+    context = {}
+
+    user = request.user
+    if user.is_autenticated:
+        return redirect("home")
+
+    if request.POST:
+        form = AccountAuthenticationForm(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+
+            if user:
+                login(request, user)
+                return redirect("home")
+    else:
+        form = AccountAuthenticationForm()
+
+    context['login_form'] = form
+    return render(request, "account/login.html", context)
