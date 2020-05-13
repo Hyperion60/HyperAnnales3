1d8350cb177a1af779b39bbcbcc6ca8453ee38e9
Hyperion <lasagnes.tomate@gmail.com>
2020-05-13 21:03:59 +0200

    django: Import error

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,11 @@
 from django import forms
-from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate
 
 from accounts.models import Account
 
 
-class RegistrationForm(UserChangeForm):
+class RegistrationForm(UserCreationForm):
     email = forms.EmailField(max_length=100, help_text='Required. Add a valid email address')
 
     class Meta:
