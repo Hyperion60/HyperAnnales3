0af35f6ed4ef070eb74b9756c9f231bc6306539b
Hyperion <lasagnes.tomate@gmail.com>
2020-05-19 23:45:05 +0200

    django: Change email backend

--- HyperAnnales/settings.py
+++ HyperAnnales/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 
 # Email settings
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'ssl0.ovh.net'
 
