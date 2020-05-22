8fb125ed3e09498029733dd0c347894fa6a1c0b3
Hyperion <lasagnes.tomate@gmail.com>
2020-05-22 15:49:37 +0200

    django(reset): fix set_password function

--- accounts/views.py
+++ accounts/views.py
@@ -177,7 +177,9 @@ def change_password(request, uidb64, token):
             user = None
             context['error'] = "Le lien n'est pas valide"
         if user is not None and account_activation_token.check_token(user, token):
-            user[0].setpassword(password1)
+            print(user[0])
+            print(type(user[0]))
+            user[0].set_password(password1)
             user[0].save(using='default')
             context['mail'] = "Votre mot de passe a bien été modifié."
             return redirect('/', context)
