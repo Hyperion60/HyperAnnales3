11a8f556180e6fe5a4102c99e3410791eb6962e6
Hyperion <lasagnes.tomate@gmail.com>
2020-05-28 22:31:52 +0200

    django(static): Add function for static admin page

--- static_files/views.py
+++ static_files/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from static_files.models import YearFile, SubjectFile, StaticContent
 
 # Create your views here.
+
+
+@login_required(login_url="/login/")
+def static_admin(request):
+    if not request.user.is_contributor:
+        raise PermissionDenied
+    context = {}
+    if request.user.is_staff:
+        context['years'] = YearFile.objects.all()
+        context['subjects'] = SubjectFile.objects.all()
+    context['contribution'] = StaticContent.objects.filter(author=request.user).order_by('desc')
+    return render(request, "static_content/admin/index.html", context)
