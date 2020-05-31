18f5defba32b6b236a3847c02c3e930b942ef298
Hyperion <lasagnes.tomate@gmail.com>
2020-05-31 18:20:10 +0200

    django(db): Move pdf_file in primary db

--- HyperAnnales/routers.py
+++ HyperAnnales/routers.py
@@ -1,33 +1,25 @@
 class HyperionRouter:
-    route_user_app = {'auth', 'contenttypes', 'accounts', 'django', 'sessions', 'admin'}
-    route_static_app = {'static_files'}
+    route_user_app = {'auth', 'contenttypes', 'accounts', 'django', 'sessions', 'admin', 'static_files'}
 
     def db_for_read(self, model, **hints):
-        if model._meta.app_label in self.route_static_app:
-            return 'pdf_ref'
-        print(model._meta.app_label)
         if model._meta.app_label in self.route_user_app:
             return 'user_ref'
         return None
 
     def db_for_write(self, model, **hints):
-        if model._meta.app_label in self.route_static_app:
-            return 'pdf_ref'
         if model._meta.app_label in self.route_user_app:
             return 'user_ref'
         return None
 
     def allow_relation(self, obj1, obj2, **hints):
         if (
-            obj1._meta.app_label in self.route_static_app or
+            obj1._meta.app_label in self.route_user_app or
             obj2._meta.app_label in self.route_user_app
         ):
             return True
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        if app_label in self.route_static_app:
-            return db == 'pdf_ref'
         if app_label in self.route_user_app:
             return db == 'user_ref'
         return None
