3d9281c74bf57cf8a2c21631b52e92f0a2ada48b
Hyperion <lasagnes.tomate@gmail.com>
2020-06-01 13:50:26 +0200

    django(add): Add function to set semester

--- static_files/views.py
+++ static_files/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
-from static_files.models import YearFile, SubjectFile, StaticContent
+from static_files.models import YearFile, SemesterFile,SubjectFile, StaticContent
 from static_files.forms import CreateYearForm
 
 # Create your views here.
@@ -25,17 +25,36 @@ def static_admin(request):
     return render(request, "static_content/admin/index.html", context)
 
 
+def __search_create_semester(semester):
+    test_s = SemesterFile.objects.filter(semester__exact=semester)
+    if not len(test_s):
+        new_semester = SemesterFile.objects.filter(semester=active_semester)
+        new_semester.save()
+        return new_semester
+    return test_s[0]
+
+
+def __create_class_year(year, active_semester):
+    test_s = __search_create_semester(active_semester)
+    test_y = YearFile.objects.filter(year__exact=year)
+    if not len(test_y):
+        new_promo = YearFile(year=year, active_semester=test_s)
+        new_promo.save()
+
+
 @login_required(login_url="/login/")
-def create_year(request):
+def set_year_semester(request):
     if not request.user.is_staff:
         raise PermissionDenied
     context = {}
     if request.POST:
         form = CreateYearForm(request.POST)
-        print(form)
-        print(form.is_valid())
         if form.is_valid():
-            print(form.cleaned_data)
+            semester = form.cleaned_data['active_semester']
+            year = form.cleaned_data['year']
+            __create_class_year(year, semester)
+            context['message'] = "Le semestre actuel a bien été actualisé."
+            return render(request, "static_content/admin/index.html", context)
     else:
         context['form'] = CreateYearForm()
     return render(request, "static_content/add/year.html", context)
