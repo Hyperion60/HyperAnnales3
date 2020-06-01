23e47420ab43a6beed44c9905a3b0a37cf536250
Hyperion <lasagnes.tomate@gmail.com>
2020-06-01 20:16:05 +0200

    django(refactor): Move class methods views in separate file

--- static_files/views.py
+++ static_files/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
-from static_files.models import YearFile, SemesterFile,SubjectFile, StaticContent
-from static_files.forms import CreateYearForm
+from static_files.models import YearFile, SemesterFile, SubjectFile, StaticContent
 
 # Create your views here.
 
@@ -24,37 +23,3 @@ def static_admin(request):
         context['contribution'] = None
     return render(request, "static_content/admin/index.html", context)
 
-
-def __search_create_semester(semester):
-    test_s = SemesterFile.objects.filter(semester__exact=semester)
-    if not len(test_s):
-        new_semester = SemesterFile(semester=semester)
-        new_semester.save()
-        return new_semester
-    return test_s[0]
-
-
-def __create_class_year(year, active_semester):
-    test_s = __search_create_semester(active_semester)
-    test_y = YearFile.objects.filter(year__exact=year)
-    if not len(test_y):
-        new_promo = YearFile(year=year, active_semester=test_s)
-        new_promo.save()
-
-
-@login_required(login_url="/login/")
-def set_year_semester(request):
-    if not request.user.is_staff:
-        raise PermissionDenied
-    context = {}
-    if request.POST:
-        form = CreateYearForm(request.POST)
-        if form.is_valid():
-            semester = form.cleaned_data['active_semester']
-            year = form.cleaned_data['year']
-            __create_class_year(year, semester)
-            context['message'] = "Le semestre actuel a bien été actualisé."
-            return render(request, "static_content/admin/index.html", context)
-    else:
-        context['form'] = CreateYearForm()
-    return render(request, "static_content/add/year.html", context)
