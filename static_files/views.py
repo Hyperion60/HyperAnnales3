854cd6f28ebb779543c18e49f2f67a294ee3fbc4
Hyperion <lasagnes.tomate@gmail.com>
2020-06-01 14:04:00 +0200

    django(form_static): Mistake

--- static_files/views.py
+++ static_files/views.py
@@ -28,7 +28,7 @@ def static_admin(request):
 def __search_create_semester(semester):
     test_s = SemesterFile.objects.filter(semester__exact=semester)
     if not len(test_s):
-        new_semester = SemesterFile.objects.filter(semester=semester)
+        new_semester = SemesterFile(semester=semester)
         new_semester.save()
         return new_semester
     return test_s[0]
