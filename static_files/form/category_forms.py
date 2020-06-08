388d92457bc8b009b896fb769c67754553d38ac9
Hyperion <lasagnes.tomate@gmail.com>
2020-06-08 19:15:33 +0200

    django(static_admin): Fix choice field and multi

--- static_files/form/category_forms.py
+++ static_files/form/category_forms.py
@@ -4,17 +4,17 @@ from static_files.models import SubjectFile, SemesterFile, YearFile
 
 class CreateCategoryForm(forms.Form):
     LIST_CAT = (
-        ('TD', 'blue'),
-        ('Documents', 'green'),
-        ('Controles', 'red'),
-        ('QCM', 'blue'),
-        ('Aide/Cours', 'green'),
+        ('blue', 'TD'),
+        ('green', 'Documents'),
+        ('red', 'Contrôles'),
+        ('blue', 'QCM'),
+        ('green', 'Aide/Cours'),
     )
 
     title = forms.CharField(max_length=150)
-    subject = forms.ModelMultipleChoiceField(queryset=SubjectFile.objects.all(), to_field_name="subject")
-    semester = forms.ModelMultipleChoiceField(queryset=SemesterFile.objects.all(), to_field_name="semester")
-    year = forms.ModelMultipleChoiceField(queryset=YearFile.objects.all(), to_field_name="year")
+    subject = forms.ModelChoiceField(queryset=SubjectFile.objects.all(), to_field_name="subject")
+    semester = forms.ModelChoiceField(queryset=SemesterFile.objects.all(), to_field_name="semester")
+    year = forms.ModelChoiceField(queryset=YearFile.objects.all(), to_field_name="year")
     category = forms.ChoiceField(choices=LIST_CAT)
 
 
