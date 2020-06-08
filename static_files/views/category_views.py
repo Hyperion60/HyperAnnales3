0aa9ed1ae439647946c11cb586e4bbefd55e7061
Hyperion <lasagnes.tomate@gmail.com>
2020-06-08 17:14:17 +0200

    django(static_admin): Add test views for category

---
+++ static_files/views/category_views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+
+from static_files.form.category_forms import CreateCategoryForm
+
+
+@login_required(login_url="/login/")
+def CreateCategoryView(request):
+    if not request.user.is_staff:
+        raise PermissionDenied
+    context = {}
+    if request.POST:
+        form = CreateCategoryForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            print(subject)
+            context['error'] = "Not implemented"
+        else:
+            context['error'] = "Form is not valid"
+    else:
+        context['form'] = CreateCategoryForm()
+    return render(request, "static_content/add/add-category.html", context)
+
