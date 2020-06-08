339ec2e3ea7ab938aa1ea180fab8b8910bdbba66
Hyperion <lasagnes.tomate@gmail.com>
2020-06-08 15:03:22 +0200

    django(static_admin): Add field_name to year forms

--- static_files/form/year_forms.py
+++ static_files/form/year_forms.py
@@ -1,14 +1,14 @@
 from django import forms
 
-from static_files.models import YearFile, SemesterFile
+from static_files.models import SemesterFile
 
 from datetime import datetime
 
 
 class CreateYearForm(forms.Form):
     year = forms.IntegerField(min_value=2014, max_value=datetime.now().year + 5)
-    active_semester = forms.ModelMultipleChoiceField(queryset=SemesterFile.objects.all(), empty_label="(Vide)")
+    active_semester = forms.ModelMultipleChoiceField(queryset=SemesterFile.objects.all(), to_field_name="semester")
 
 
 class SetYearSemester(forms.Form):
-    active_semester = forms.ModelMultipleChoiceField(queryset=SemesterFile.objects.all())
+    active_semester = forms.ModelMultipleChoiceField(queryset=SemesterFile.objects.all(), to_field_name="semester")
