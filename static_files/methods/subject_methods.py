de018a8e891944931303863b7cbfb10a018c3da8
Hyperion <lasagnes.tomate@gmail.com>
2020-06-08 14:42:30 +0200

    django(static_admin): Add subject form in separate file

---
+++ static_files/methods/subject_methods.py
@@ -0,0 +1,11 @@
+from django import forms
+
+from static_files.models import YearFile, SemesterFile
+
+
+class SetSubject(forms.Form):
+    subject = forms.CharField(max_length=100)
+    semester = forms.ModelMultipleChoiceField(queryset=SemesterFile.objects.all(), to_field_name="semester")
+    year = forms.ModelMultipleChoiceField(queryset=YearFile.objects.all(), to_field_name="year")
+
+
