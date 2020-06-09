9317a6a2e5ba95d8dcce5061a98782cef3906613
Hyperion <lasagnes.tomate@gmail.com>
2020-06-09 15:31:10 +0200

    django(static_admin): Remove empty label

--- static_files/form/category_forms.py
+++ static_files/form/category_forms.py
@@ -11,10 +11,9 @@ class CreateCategoryForm(forms.Form):
         ('green', 'Aide/Cours'),
     )
 
-    title = forms.CharField(max_length=150)
-    subject = forms.ModelChoiceField(queryset=SubjectFile.objects.all().order_by('subject'), empty_label="Matière")
-    semester = forms.ModelChoiceField(queryset=SemesterFile.objects.all().order_by('semester'), empty_label="Semestre")
-    year = forms.ModelChoiceField(queryset=YearFile.objects.all().order_by('year'), empty_label="Année")
+    subject = forms.ModelChoiceField(queryset=SubjectFile.objects.all().order_by('subject'))
+    semester = forms.ModelChoiceField(queryset=SemesterFile.objects.all().order_by('semester'))
+    year = forms.ModelChoiceField(queryset=YearFile.objects.all().order_by('year'))
     category = forms.ChoiceField(choices=LIST_CAT)
 
 
