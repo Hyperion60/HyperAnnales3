12a4acc57cc3a34e1d48d78344b9a9e79054b0c0
Hyperion <lasagnes.tomate@gmail.com>
2020-06-10 13:20:26 +0200

    django(static_admin-semester): Remove useless arg in save()

--- static_files/methods/semester_methods.py
+++ static_files/methods/semester_methods.py
@@ -7,7 +7,7 @@ def create_semester(semester):
     test_s = SemesterFile.objects.filter(semester=semester)
     if not len(test_s):
         new_semester = SemesterFile(semester=semester)
-        new_semester.save(using='pdf_ref')
+        new_semester.save()
         return new_semester
     return test_s[0]
 
