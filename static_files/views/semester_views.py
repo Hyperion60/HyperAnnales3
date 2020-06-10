767fccffbd4fb99f802f0d33653a52853271921c
Hyperion <lasagnes.tomate@gmail.com>
2020-06-10 13:13:44 +0200

    django(static_admin-semester): Add view function to create semester

---
+++ static_files/views/semester_views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+
+from static_files.methods.semester_methods import create_semester
+
+
+@login_required(login_url="/login/")
+def CreateSemesterView(request):
+    if not request.user.is_staff:
+        raise PermissionDenied
+    context = {}
+    error = False
+    if request.POST:
+        semester = 0
+        try:
+            semester = int(request.POST['semester'])
+        except KeyError:
+            context['error'] = "Entrez un numéro de semestre"
+            error = True
+        if not error:
+            create_semester(semester)
+            return render(request, "static_content/admin/message-template.html",
+                          {'message': "Le semestre a bien été créé"})
+    if not error:
+        context['error'] = None
+    return render(request, "static_content/add/add-semester.html", context)
