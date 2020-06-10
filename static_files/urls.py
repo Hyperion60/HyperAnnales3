0c28d7d4fd4e6a86da08e7f3de1225a288ead4a1
Hyperion <lasagnes.tomate@gmail.com>
2020-06-10 12:53:57 +0200

    django(static_admin-subject): Add subject creation in urls.py

--- static_files/urls.py
+++ static_files/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from static_files.views import admin_views, category_views
+from static_files.views import admin_views, category_views, subject_views
 
 
 urlpatterns = [
     path('admin/', admin_views.static_admin),
     path('add/category/', category_views.CreateCategoryView),
+    path('add/subject/', subject_views.CreateSubjectView),
 ]
