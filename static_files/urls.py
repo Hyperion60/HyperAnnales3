4246911343a8beb39578979132fe8aefa2aa1778
Hyperion <lasagnes.tomate@gmail.com>
2020-06-10 13:15:40 +0200

    django(static_admin-semester): Add semester in urls.py

--- static_files/urls.py
+++ static_files/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from static_files.views import admin_views, category_views, subject_views
+from static_files.views import admin_views,\
+    category_views,\
+    subject_views,\
+    semester_views
 
 
 urlpatterns = [
     path('admin/', admin_views.static_admin),
     path('add/category/', category_views.CreateCategoryView),
+    path('add/semester/', semester_views.CreateSemesterView),
     path('add/subject/', subject_views.CreateSubjectView),
 ]
