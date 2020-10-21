2d03e10ebf2395fbb42c1d0d15850782ff93371f
Remi Maubanc <remi.maubanc@epita.fr>
2020-10-21 12:39:34 +0000

    methods(models): Add methods to create: Static Content Static File Extension

--- static_files/models.py
+++ static_files/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from accounts.models import Account
+from HyperAnnales.settings import MEDIA_ROOT as root_path
+from static_files.methods.annexe_methods import *
 
 import os
 from datetime import datetime
@@ -16,6 +18,9 @@ class School(models.Model):
     def __str__(self):
         return str(self.school)
 
+    def count(self):
+        return school_file_count(self)
+
 class SemesterFile(models.Model):
     semester = models.IntegerField(unique=True)
 
@@ -30,12 +35,16 @@ class YearFile(models.Model):
     def __str__(self):
         return str(self.year)
 
+    def count(self, school):
+        return school_file_count__year(school, self)
+
 
 class SubjectFile(models.Model):
     subject = models.CharField(max_length=100, unique=False)
     semester = models.ForeignKey(SemesterFile, models.CASCADE, default=None)
     year = models.ForeignKey(YearFile, models.CASCADE, default=None)
-    school = models.ForeignKey(School, models.CASCADE, default=None)
+    location = models.ForeignKey(School, models.CASCADE, default=None)
+    count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.subject
@@ -72,7 +81,7 @@ class StaticFile(models.Model):
     filename = models.CharField(max_length=255, default=None)
     weight = models.IntegerField()
     author = models.ForeignKey(Account, models.CASCADE, default=None)
-    random_key = models.IntegerField(unique=True)
+    randomkey = models.IntegerField(unique=True)
     extension = models.ForeignKey(ExtensionFile, models.CASCADE, default=None)
     content = models.ForeignKey('StaticContent', models.CASCADE, default=None)
 
@@ -95,30 +104,29 @@ def create_subject(request):
     subject_s = SubjectFile.objects.filter(subject__exact=subject)
     if not len(subject_s):
         new_subject = SubjectFile(subject=subject)
-        new_subject.save(using='pdf_ref')
+        new_subject.save()
 
 
-def create_file(request, raw_path):
-    year = request.POST['year']
-    semester = request.POST['semester']
-    subject = request.POST['subject']
-    name = request.POST['name']
-    date = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
-    weight = round(os.stat(raw_path).st_size / 1024)
-    author = request.user
-    key = 0
-    while key == 0 and len(StaticContent.objects.filter(random_key__exact=key)):
-        key = randint(1, 999999)
-    path = STATIC_PATH + str(year) + "/" + str(semester) + "/" + str(subject) +\
-                    "/" + str(name) + "-" + str(key) + ".pdf"
-    file = StaticContent(year=year,
-                         semester=semester,
-                         subject=subject,
-                         name=name,
-                         date=date,
-                         weight=weight,
-                         author=author,
-                         ramdom_key=key)
-    file.save(using='pdf_ref')
-    copyfile(raw_path, path)
-    os.remove(raw_path)
+def create_file(context, request):
+    # Check extension (create if necessary)
+    context['extensionfile'] = check_extension(context['extension'])
+    new_staticFile = StaticFile(path=context['path'],
+                                date=datetime.now().strftime("%d-%m-%Y_%H:%M:%S"),
+                                filename=context['filename'],
+                                weight=round(os.path(root_path + context['raw_path']).st_size / 1024),
+                                author=request.user,
+                                randomkey=context['key'],
+                                extension=ExtensionFile.objects.filter(extension__exact=context['extension'])[0])
+    new_staticFile.save()
+
+    new_staticContent = StaticContent(school=context['school'],
+                                      year=context['year'],
+                                      semester=context['semester'],
+                                      subject=context['subject'],
+                                      category=context['category'],
+                                      name=context['filename'],
+                                      file=new_staticFile)
+    new_staticContent.save()
+    new_staticFile.content = new_staticContent
+    new_staticFile.save()
+
