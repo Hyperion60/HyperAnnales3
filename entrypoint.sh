7ad5bdc721e5b95652074be130630a789d977662
Hyperion <mire.maubanc@gmail.com>
2021-08-30 00:49:13 +0200

    start(method): Add migration commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-
 set -e
 
 sleep 5
+
+python manage.py makemigrations
+python manage.py makemigrations static_files
+python manage.py migrate --database=user_ref
+
 uwsgi --wsgi-file "/home/HyperAnnales/HyperAnnales/wsgi.py" --http-socket 0.0.0.0:6094 --chdir "/home/HyperAnnales" --show-config
