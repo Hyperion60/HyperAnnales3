557a7aed4b5bf15deff34e4aad5d6bef0bae3025
Hyperion <mire.maubanc@gmail.com>
2021-08-30 00:49:51 +0200

    start(method): Add method for start app

--- static_files/apps.py
+++ static_files/apps.py
@@ -3,3 +3,43 @@ from django.apps import AppConfig
 
 class StaticFilesConfig(AppConfig):
     name = 'static_files'
+
+    def ready(self):
+        from .models import School, YearFile, SemesterFile, ContentColor, CategoryColor
+        from methods.year_methods import create_year
+        from methods.category_methods import COLOR_CATEGORY
+        from methods.file_methods import COLOR_FILE
+        try:
+            School.objects.get(school__exact="EPITA")
+        except School.DoesNotExist:
+            School(school="EPITA").save()
+        try:
+            School.objects.get(school__exact="ESIEE")
+        except School.DoesNotExist:
+            School(school="ESIEE").save()
+
+        for semester in range(1, 11):
+            try:
+                SemesterFile.objects.get(semester__exact=semester)
+            except SemesterFile.DoesNotExist:
+                SemesterFile(semester=semester).save()
+
+        active_semester = 9
+        for year in range(2022, 2027):
+            try:
+                YearFile.objects.get(year__exact=year)
+            except YearFile.DoesNotExist:
+                create_year(year, SemesterFile.objects.get(semester__exact=active_semester))
+            active_semester -= 2
+
+        for (type, color) in COLOR_FILE:
+            try:
+                ContentColor.objects.get(type__exact=type)
+            except ContentColor.DoesNotExist:
+                ContentColor(type=type, color=color).save()
+
+        for (type, color) in COLOR_CATEGORY:
+            try:
+                CategoryColor.objects.get(type__exact=type)
+            except CategoryColor.DoesNotExist:
+                CategoryColor(type=type, color=color)
