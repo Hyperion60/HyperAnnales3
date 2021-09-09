e5f974451767d4e2cb1266c54f2f1e72713333ce
Hyperion <mire.maubanc@gmail.com>
2021-09-09 10:57:26 +0200

    information(template): Fix url path to add infos

--- static_files/urls.py
+++ static_files/urls.py
@@ -5,12 +5,15 @@ from static_files.views import admin_views,\
     semester_views,\
     year_views,\
     base_template,\
-    file_views
+    file_views, \
+    information_views
 
 
 urlpatterns = [
     path('admin/', admin_views.static_admin),
     path('add/category/', category_views.CreateCategoryView),
+    path('add/information/<str:school>/<int:year>/', information_views.CreateInformation),
+    path('add/information/<str:school>/', information_views.CreateInformation),
     path('add/file/', file_views.init_addfile_view),
     path('add/semester/', semester_views.CreateSemesterView),
     path('add/subject/', subject_views.CreateSubjectView),
