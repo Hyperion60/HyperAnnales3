22354dae86d88ddf570805fd86fd19344ac2bcf4
Hyperion <mire.maubanc@gmail.com>
2021-09-10 11:27:49 +0200

    bulletin(add): Fix exception

--- static_files/views/bulletin_views.py
+++ static_files/views/bulletin_views.py
@@ -6,6 +6,8 @@ from static_files.methods.bulletin_methods import create_information
 
 def CreateInformation(request, school, year=None):
     context = {'errors': []}
+    context['school'] = school
+    context['year'] = year
     if request.POST:
         if create_information(request, context):
             context['next'] = "/{}/".format(school)
@@ -17,6 +19,4 @@ def CreateInformation(request, school, year=None):
         context['title'] = request.POST['title']
     except MultiValueDictKeyError:
         pass
-    context['school'] = school
-    context['year'] = year
     return render(request, "static_content/add/add-information.html", context)
