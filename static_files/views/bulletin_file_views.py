71a2aac03677de774ca6d1e34cfb629963dac73c
Hyperion <mire.maubanc@gmail.com>
2021-09-22 17:27:23 +0200

    unsecure file: Add function to update bulletin file

--- static_files/views/bulletin_file_views.py
+++ static_files/views/bulletin_file_views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.utils.datastructures import MultiValueDictKeyError
 
-from static_files.models import Bulletin, ExtensionFile
-from static_files.methods.unsecure_file_methods import add_unsecured_file
+from static_files.models import Bulletin, UnsecureFile, ExtensionFile
+from static_files.methods.unsecure_file_methods import add_unsecured_file, update_unsecure_file
 from static_files.views.annexe_functions import create_list_extension
 
 
@@ -52,3 +52,59 @@ def add_file_bulletin(request, pk):
     context['list_extension'] = create_list_extension()
 
     return render(request, "static_content/add/add-bulletin-file.html", context)
+
+
+def update_bulletin_file(request, pk):
+    context = {
+        'errors': [],
+        'next': request.GET.get('next', ''),
+    }
+    try:
+        context['file'] = UnsecureFile.objects.get(pk=pk)
+    except UnsecureFile.DoesNotExist:
+        context['errors'].appand("Le fichier demandé est introuvable. La clé primaire n'existe pas.")
+    try:
+        context['bulletin'] = Bulletin.objects.get(pk=context['file'].bulletin.pk)
+    except Bulletin.DoesNotExist:
+        context['errors'].append("Le bulletin demandé est introuvable. La clé primaire n'existe pas.")
+
+    if request.POST:
+        try:
+            context['url'] = request.POST['url']
+            context['type'] = "url"
+        except MultiValueDictKeyError:
+            context['type'] = "file"
+
+        if context['type'] == "file" and context['file'].filename != request.POST['file_name']:
+            try:
+                context['file_post'] = request.FILES['file']
+            except MultiValueDictKeyError:
+                context['errors'].append("Vous devez soit entrer une URL soit uploader un fichier.")
+
+        elif context['file'].filename == request.POST['file_name']:
+            context['type'] = None
+
+        try:
+            context['extension'] = ExtensionFile.objects.get(pk=request.POST['extension'])
+        except MultiValueDictKeyError:
+            context['errors'].append("Champ d'extension manquant.")
+        except ExtensionFile.DoesNotExist:
+            context['errors'].append("L'extension demandée est introuvable. La clé primaire n'existe pas.")
+
+        update_unsecure_file(context)
+        return render(request, "static_content/admin/message_template.html", context)
+
+    context['extensions'] = ExtensionFile.objects.exclude(pk=context['file'].extension)
+    context['list_extension'] = create_list_extension()
+    file = {
+        'name': context['file'].name,
+        'extension': context['file'].extension,
+    }
+    if context['file'].extension.extension == "url":
+        file['url'] = context['file'].url
+        file['filename'] = None
+    else:
+        file['filename'] = context['file'].filename
+        file['url'] = None
+
+    return render(request, "static_content/change/change-unsecured-file.html", context)
