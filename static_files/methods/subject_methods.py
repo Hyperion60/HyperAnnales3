faf630ce6526bc5c5f8814befe345643a8d4de68
Rémi <lasagnes.tomate@gmail.com>
2021-12-23 01:02:53 +0100

    subject: Add modify and delete methods+views

--- static_files/methods/subject_methods.py
+++ static_files/methods/subject_methods.py
@@ -1,4 +1,4 @@
-from static_files.models import SubjectFile, SemesterFile, YearFile
+from static_files.models import SubjectFile, SemesterFile, YearFile, CategoryFile, StaticContent, StaticFile
 
 
 # context(dict), subject(str), semester(pk), year(pk), school(pk)
@@ -24,6 +24,39 @@ def CreateSubject(context, subject, semester, year, school):
                 context['new_subject_obj'] = list_subj[0]
                 context['error'] = "La matière existe deja"
     return context
+
+
+def UpdateSubject(request, context):
+    if not request.user.is_staff:
+        return False
+
+    try:
+        context['subject'].subject = context['title']
+        context['subject'].semester = context['semester']
+        context['subject'].year = context['year']
+        context['subject'].location = context['school']
+        context['subject'].save()
+    except:
+        context['errors'].append("Echec de la mise à jour du bulletin")
+        return False
+    return True
+
+
+def DeleteSubject(request, context):
+    if not request.user.is_staff:
+        return False
+
+    try:
+        for category in CategoryFile.objects.filter(subject_id__exact=context['subject'].pk):
+            for static in StaticContent.objects.filter(category_id__exact=category.pk):
+                static.file.delete()
+                static.delete()
+            category.delete()
+        context['subject'].delete()
+    except:
+        context['errors'].append("Impossible de supprimer la matière ou ses dépendances.")
+        return False
+    return True
 
 
 def CleanSubject():
--- static_files/views/subject_views.py
+++ static_files/views/subject_views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
+from django.utils.datastructures import MultiValueDictKeyError
 
-from static_files.models import School, CategoryFile, StaticContent
+from static_files.models import School, YearFile, SemesterFile, SubjectFile
 from static_files.form.subject_forms import SetSubject
-from static_files.methods.subject_methods import CreateSubject
+from static_files.methods.subject_methods import CreateSubject, UpdateSubject, DeleteSubject
 
 
 @login_required(login_url="/login/")
@@ -39,3 +40,93 @@ def CreateSubjectView(request):
     if not error:
         context['error'] = None
     return render(request, "static_content/add/add-subject.html", context)
+
+
+@login_required(login_url="/login/")
+def UpdateSubjectView(request, pk):
+    context = {
+        'errors': [],
+        'next': request.GET.get('next', '')
+    }
+
+    try:
+        context['subject'] = SubjectFile.objects.get(pk=pk)
+    except SubjectFile.DoesNotExist:
+        context['errors'].append("La clé demandée n'existe pas.")
+        return render(request, "static_content/admin/message_template.html", context)
+
+    if not request.user.is_staff:
+        context['errors'].append("Vous n'avez pas l'authorisation de modifier cette instance.")
+        return render(request, "static_content/admin/message_template.html", context)
+
+    context['years'] = YearFile.objects.all().order_by("year")
+    context['semesters'] = SemesterFile.objects.all().order_by("semester")
+    context['schools'] = School.objects.all().order_by("school")
+
+    if request.POST:
+        try:
+            context['title'] = request.POST['title']
+        except MultiValueDictKeyError:
+            context['errors'].append("Champ 'titre' manquant.")
+
+        try:
+            context['school'] = School.objects.get(pk=request.POST['school'])
+        except MultiValueDictKeyError:
+            context['errors'].append("Champ 'école' manquant.")
+        except School.DoesNotExist:
+            context['errors'].append("L'école demandée n'existe pas. Clé primaire introuvable.")
+
+        try:
+            context['year'] = YearFile.objects.get(pk=request.POST['year'])
+        except MultiValueDictKeyError:
+            context['errors'].append("Champ 'année' manquant.")
+        except YearFile.DoesNotExist:
+            context['errors'].append("L'année demandée n'existe pas. Clé primaire introuvable.")
+
+        try:
+            context['semester'] = SemesterFile.objects.get(pk=request.POST['semester'])
+        except MultiValueDictKeyError:
+            context['errors'].append("Champ 'semestre' manquant.")
+        except SemesterFile.DoesNotExist:
+            context['errors'].append("Le semestre demandé n'existe pas. Clé primaire introuvable.")
+
+        if not len(context['subject']):
+            context['errors'].append("Le titre de la matière ne peut être vide.")
+
+        if len(context['errors']):
+            return render(request, "static_content/change/change-subject.html", context)
+
+        if UpdateSubject(request, context):
+            context['message'] = "Modification effectuée avec succès."
+            return render(request, "static_content/admin/message_template.html", context)
+        return render(request, "static_content/change/change-subject.html", context)
+    return render(request, "static_content/change/change-subject.html", context)
+
+
+@login_required(login_url="/login/")
+def DeleteSubjectView(request, pk):
+    context = {
+        'errors': [],
+        'next': request.GET.get('next', '')
+    }
+
+    if not request.user.is_staff:
+        context['errors'].append("Vous n'avez l'authorisation de supprimer cette instance.")
+        return render(request, "static_content/admin/message_template.html", context)
+
+    try:
+        context['subject'] = SubjectFile.objects.get(pk=pk)
+    except SubjectFile.DoesNotExist:
+        context['errors'].append("La matière demandée n'existe pas. Clé primaire introuvable.")
+        return render(request, "static_content/admin/message_template.html", context)
+
+    if request.POST:
+        if request.POST.get('cancel', None):
+            return redirect(context['next'])
+        if DeleteSubject(request, context):
+            context['message'] = "Suppression effectuée avec succès."
+        else:
+            context['errors'].append("Echec de la suppression de la matière.")
+        return render(request, "static_content/admin/message_template.html", context)
+
+    return render(request, "static_content/del/del-subject.html", context)
