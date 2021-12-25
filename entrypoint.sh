d1164a9b16cba0e4f5a9823491768481f3455227
Hyperion <35610696+Hyperion60@users.noreply.github.com>
2021-12-25 01:18:03 +0100

    entrypoint: Handle apps.py bug when the db is just created

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,19 @@ set -e
 
 sleep 5
 
+# Handle when the database just created
+mkdir /tmp/static
+mv static_files/apps.py /tmp/static/
+mv accounts/apps.py /tmp/
+# ----
+
 cd HyperAnnales && python manage.py makemigrations
 python manage.py makemigrations static_files
 python manage.py migrate --database=user_ref
+
+# ----
+mv /tmp/static/apps.py static_files/
+mv /tmp/apps.py accounts/
+# ----
 
 uwsgi --wsgi-file "/home/HyperAnnales/HyperAnnales/wsgi.py" --http-socket 0.0.0.0:6094 --chdir "/home/HyperAnnales" --show-config
